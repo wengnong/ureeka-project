285243e48d1f25006a9ae8fec6cb0163016ab0d1
Winona <155759283+wengnong@users.noreply.github.com>
2025-05-23 17:36:08 +0700

    minor changes

--- sesi4/src/app/page.tsx
+++ sesi4/src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col gap-6 justify-center items-center">
         <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-3xl leading-none">Hello, I&apos;m Winona.</h1>
-          <p className="text-sm leading-none">This is my portfolio.</p>
+          <h1 className="text-5xl leading-none font-bold">Hello, I&apos;m Winona.</h1>
+          <p className="text-sm leading-none italic">{`< this is my portfolio. >`}</p>
         </div>
 
         <ul className="flex flex-row gap-8 text-gray-600">
@@ -15,7 +15,7 @@ export default function Home() {
           <li className="hover:text-gray-300 transition-all duration-300"><Link href='contact'>contact</Link></li>
         </ul>
 
-        <p className="hover:text-blue-300 transition-all duration-300"><Link href='weather'>check the weather here!</Link></p>
+        <p className="border-2 text-xs hover:text-blue-300 hover:bg-blue-300/10 px-3 py-1 rounded-lg transition-all duration-300"><Link href='weather'>psst..! check the weather here!</Link></p>
     </div>
   );
 }
--- sesi4/src/app/weather/page.tsx
+++ sesi4/src/app/weather/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
 interface Post {
     request: {
         type: string,
@@ -62,12 +65,24 @@ export default async function WeatherPage() {
     const data = await fetch('https://api.weatherstack.com/current?access_key=246391101c588568904ba23608394ab6&query=Tangerang')
     const post: Post = await data.json()
 
+    if (!data.ok) {
+        return { message: "Failed to create post. "}
+    }
+
     return (
-        <div className="min-h-screen w-full flex justify-center items-center">
-            <div className="flex justify-center items-center">
+        <div className="min-h-screen w-full flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center border-2 px-12 py-18 border-white">
                 <h1 className="text-xl text-center">Weather in <br /> <span className="text-4xl text-blue-500 font-semibold uppercase">{post.location.name}</span></h1>
-                
+                <p className="text-blue-300">{post.location.region}, {post.location.country}</p>
+
+                <p className="text-center text-xs">Local Time <br /> {post.location.localtime}</p>
+                <div className="mt-6 text-sm flex flex-row gap-20 justify-between">
+                    <span>Latitude</span>
+                    <span>{post.location.lat}</span>
+                </div>
             </div>
+
+            <Button className="mt-10" variant="ghost"><Link href="/">go back to home page</Link></Button>
         </div>
     )
 }
